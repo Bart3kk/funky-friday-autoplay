0ee9cd7bfaad838453edefa6cd784fe8d8ff540c
Bart3kk <115643187+Bart3kk@users.noreply.github.com>
2023-05-13 18:08:06 +0000

    Update metadata.lua

--- metadata.lua
+++ metadata.lua
@@ -1,5 +1,5 @@
 return {
 	version = '2.2b MOBILE',
 	updated = '5/13/23',
-	message = 'MOBILE SUPPORT ADDED!'
+	message = 'MOBILE SUPPORT ADDED! (configs and themes tabs are broken, im working on fixing them)'
 }
