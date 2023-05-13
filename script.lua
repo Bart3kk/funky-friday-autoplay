528218b8a5905169afe784b6fe41564c4a1c7e55
Bart3kk <115643187+Bart3kk@users.noreply.github.com>
2023-05-13 16:45:59 +0000

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,9 +1,9 @@
---Credits to ShowerHead-FluxTeam for the ported version of Wally-RBLX's Funky Friday AutoPlay script
---Credits to mstudio45 for the ported UI lib of Linoria
---Credits to Wally-RBLX and his devs for making this amazing script
---I DO NOT OWN ANY OF THIS CODE. CREDITS TO THIS CODE AND UI LIBS ETC GO TO THEIR OWNERS.
-
---Wally RBLX's Funky Friday AutoPlay script mobile port. (READ LINES ABOVE!)
+print("Credits to ShowerHead-FluxTeam for the ported version of Wally-RBLX's Funky Friday AutoPlay script")
+print("Credits to mstudio45 for the ported UI lib of Linoria")
+print("Credits to Wally-RBLX and his devs for making this amazing script")
+print("I DO NOT OWN ANY OF THIS CODE. CREDITS TO THIS CODE AND UI LIBS ETC GO TO THEIR OWNERS.")
+print(" ")
+print("Wally RBLX's Funky Friday AutoPlay script mobile port. (READ LINES ABOVE!)")
 
 local start = tick()
 local client = game:GetService('Players').LocalPlayer;
