08ced025a76bf6e26047eba211f804fc7fa0b189
Bart3kk <115643187+Bart3kk@users.noreply.github.com>
2023-05-14 16:55:54 +0200

    Update dev.lua

--- dev.lua
+++ dev.lua
@@ -87,21 +87,23 @@ if type(getinfo) ~= 'function' then
     end
 end
 
+local Repository = 'https://raw.githubusercontent.com/mstudio45/LinoriaLib/dev/'
+
 local IsMobile = false;
 local DevicePlatform = Enum.Platform.None;
 pcall(function() DevicePlatform = UserInputService:GetPlatform(); end);
 IsMobile = (DevicePlatform == Enum.Platform.Android or DevicePlatform == Enum.Platform.IOS);
 
-local Library = nil
+local UI = nil
 if IsMobile then
-    Library = urlLoad("https://raw.githubusercontent.com/mstudio45/LinoriaLib/dev/TestMobileSupport.lua")
+    UI = urlLoad("https://raw.githubusercontent.com/mstudio45/LinoriaLib/dev/TestMobileSupport.lua")
 else
-    Library = urlLoad("https://raw.githubusercontent.com/mstudio45/LinoriaLib/dev/Library.lua")
+    UI = urlLoad("https://raw.githubusercontent.com/mstudio45/LinoriaLib/dev/Library.lua")
 end
 
 local themeManager = urlLoad("https://raw.githubusercontent.com/wally-rblx/LinoriaLib/main/addons/ThemeManager.lua")
 
-local metadata = urlLoad("https://raw.githubusercontent.com/Bart3kk/funky-friday-autoplay/main/metadata.lua")
+local metadata = urlLoad("https://raw.githubusercontent.com/wally-rblx/funky-friday-autoplay/main/metadata.lua")
 local httpService = game:GetService('HttpService')
 
 local framework, scrollHandler, network
@@ -487,6 +489,172 @@ do
     end)
 end
 
+local SaveManager = {} do
+    SaveManager.Ignore = {}
+    SaveManager.Parser = {
+        Toggle = {
+            Save = function(idx, object) 
+                return { type = 'Toggle', idx = idx, value = object.Value } 
+            end,
+            Load = function(idx, data)
+                if Toggles[idx] then 
+                    Toggles[idx]:SetValue(data.value)
+                end
+            end,
+        },
+        Slider = {
+            Save = function(idx, object)
+                return { type = 'Slider', idx = idx, value = tostring(object.Value) }
+            end,
+            Load = function(idx, data)
+                if Options[idx] then 
+                    Options[idx]:SetValue(data.value)
+                end
+            end,
+        },
+        Dropdown = {
+            Save = function(idx, object)
+                return { type = 'Dropdown', idx = idx, value = object.Value, mutli = object.Multi }
+            end,
+            Load = function(idx, data)
+                if Options[idx] then 
+                    Options[idx]:SetValue(data.value)
+                end
+            end,
+        },
+        ColorPicker = {
+            Save = function(idx, object)
+                return { type = 'ColorPicker', idx = idx, value = object.Value:ToHex() }
+            end,
+            Load = function(idx, data)
+                if Options[idx] then 
+                    Options[idx]:SetValueRGB(Color3.fromHex(data.value))
+                end
+            end,
+        },
+        KeyPicker = {
+            Save = function(idx, object)
+                return { type = 'KeyPicker', idx = idx, mode = object.Mode, key = object.Value }
+            end,
+            Load = function(idx, data)
+                if Options[idx] then 
+                    Options[idx]:SetValue({ data.key, data.mode })
+                end
+            end,
+        }
+    }
+
+    function SaveManager:Save(name)
+        local fullPath = 'funky_friday_autoplayer/configs/' .. name .. '.json'
+
+        local data = {
+            version = 2,
+            objects = {}
+        }
+
+        for idx, toggle in next, Toggles do
+            if self.Ignore[idx] then continue end
+            table.insert(data.objects, self.Parser[toggle.Type].Save(idx, toggle))
+        end
+
+        for idx, option in next, Options do
+            if not self.Parser[option.Type] then continue end
+            if self.Ignore[idx] then continue end
+
+            table.insert(data.objects, self.Parser[option.Type].Save(idx, option))
+        end 
+
+        local success, encoded = pcall(httpService.JSONEncode, httpService, data)
+        if not success then
+            return false, 'failed to encode data'
+        end
+
+        writefile(fullPath, encoded)
+        return true
+    end
+
+    function SaveManager:Load(name)
+        local file = 'funky_friday_autoplayer/configs/' .. name .. '.json'
+        if not isfile(file) then return false, 'invalid file' end
+
+        local success, decoded = pcall(httpService.JSONDecode, httpService, readfile(file))
+        if not success then return false, 'decode error' end
+        if decoded.version ~= 2 then return false, 'invalid version' end
+
+        for _, option in next, decoded.objects do
+            if self.Parser[option.type] then
+                self.Parser[option.type].Load(option.idx, option)
+            end
+        end
+
+        return true
+    end
+
+    function SaveManager.Refresh()
+        local list = listfiles('funky_friday_autoplayer/configs')
+
+        local out = {}
+        for i = 1, #list do
+            local file = list[i]
+            if file:sub(-5) == '.json' then
+                -- i hate this but it has to be done ...
+
+                local pos = file:find('.json', 1, true)
+                local start = pos
+
+                local char = file:sub(pos, pos)
+                while char ~= '/' and char ~= '\\' and char ~= '' do
+                    pos = pos - 1
+                    char = file:sub(pos, pos)
+                end
+
+                if char == '/' or char == '\\' then
+                    table.insert(out, file:sub(pos + 1, start - 1))
+                end
+            end
+        end
+        
+        Options.ConfigList.Values = out;
+        Options.ConfigList:SetValues()
+        Options.ConfigList:Display()
+
+        return out
+    end
+
+    function SaveManager:Delete(name)
+        local file = 'funky_friday_autoplayer/configs/' .. name .. '.json'
+        if not isfile(file) then return false, string.format('Config %q does not exist', name) end
+
+        local succ, err = pcall(delfile, file)
+        if not succ then
+            return false, string.format('error occured during file deletion: %s', err)
+        end
+
+        return true
+    end
+
+    function SaveManager:SetIgnoreIndexes(list)
+        for i = 1, #list do 
+            table.insert(self.Ignore, list[i])
+        end
+    end
+
+    function SaveManager.Check()
+        local list = listfiles('funky_friday_autoplayer/configs')
+
+        for _, file in next, list do
+            if isfolder(file) then continue end
+
+            local data = readfile(file)
+            local success, decoded = pcall(httpService.JSONDecode, httpService, data)
+
+            if success and type(decoded) == 'table' and decoded.version ~= 2 then
+                pcall(delfile, file)
+            end
+        end
+    end
+end
+
 local Window = UI:CreateWindow({
     Title = string.format('funky friday autoplayer - version %s | updated: %s', metadata.version, metadata.updated),
     AutoShow = true,
@@ -563,10 +731,7 @@ Groups.Credits = Tabs.Miscellaneous:AddRightGroupbox('Credits')
 
     addRichText(Groups.Credits:AddLabel('<font color="#3da5ff">wally</font> - script'))
     addRichText(Groups.Credits:AddLabel('<font color="#de6cff">Sezei</font> - contributor'))
-    addRichText(Groups.Credits:AddLabel('<font color="#3da5ff">Bart3kk</font> - port to universal platform'))
-    addRichText(Groups.Credits:AddLabel('<font color="#de6cff">mstudio45</font> - port of Linoria UI Lib'))
-    Groups.Credits:AddLabel('ShowerHead-FluxTeam - first port, idea')
-    Groups.Credits:AddLabel('Inori - PC only ui library')
+    Groups.Credits:AddLabel('Inori - ui library')
     Groups.Credits:AddLabel('Jan - old ui library')
 
 
