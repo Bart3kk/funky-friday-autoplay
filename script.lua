d46203e239f558fdb52c785f446e02f46420b775
Bart3kk <115643187+Bart3kk@users.noreply.github.com>
2023-05-15 20:34:00 +0200

    Update script.lua

--- script.lua
+++ script.lua
@@ -5,13 +5,6 @@ print("I DO NOT OWN ANY OF THIS CODE. CREDITS TO THIS CODE AND UI LIBS ETC GO TO
 print(" ")
 print("Wally RBLX's Funky Friday AutoPlay script mobile port. (READ LINES ABOVE!)")
 
-print("Credits to ShowerHead-FluxTeam for the ported version of Wally-RBLX's Funky Friday AutoPlay script")
-print("Credits to mstudio45 for the ported UI lib of Linoria")
-print("Credits to Wally-RBLX and his devs for making this amazing script")
-print("I DO NOT OWN ANY OF THIS CODE. CREDITS TO THIS CODE AND UI LIBS ETC GO TO THEIR OWNERS.")
-print(" ")
-print("Wally RBLX's Funky Friday AutoPlay script mobile port. (READ LINES ABOVE!)")
-
 local start = tick()
 local client = game:GetService('Players').LocalPlayer;
 local set_identity = (type(syn) == 'table' and syn.set_thread_identity) or setidentity or setthreadcontext
