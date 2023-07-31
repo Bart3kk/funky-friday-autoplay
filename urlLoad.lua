4dda7accd74c6f3dd0b39aa62c976000edc28700
Bart3kk <115643187+Bart3kk@users.noreply.github.com>
2023-07-31 15:42:22 +0200

    Create urlLoad.lua
    Compressed URLLoad function

---
+++ urlLoad.lua
@@ -0,0 +1,26 @@
+local function urlLoad(url)
+    local success, result
+
+    if shared.__urlcache[url] then
+        success, result = true, shared.__urlcache[url]
+    else
+        success, result = pcall(game.HttpGet, game, url)
+    end
+
+    if (not success) then
+        return fail(string.format('Failed to GET url %q for reason: %q', url, tostring(result)))
+    end
+
+    local fn, err = loadstring(result)
+    if (type(fn) ~= 'function') then
+        return fail(string.format('Failed to loadstring url %q for reason: %q', url, tostring(err)))
+    end
+
+    local results = { pcall(fn) }
+    if (not results[1]) then
+        return fail(string.format('Failed to initialize url %q for reason: %q', url, tostring(results[2])))
+    end
+
+    shared.__urlcache[url] = result
+    return unpack(results, 2)
+end
