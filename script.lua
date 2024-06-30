61d9477d38f6082e6ba546fff5911a157b0ccb60
Bart3kk <115643187+Bart3kk@users.noreply.github.com>
2024-06-30 14:54:26 +0200

    Update script.lua

--- script.lua
+++ script.lua
@@ -97,8 +97,8 @@ local IsMobile = false;
 local DevicePlatform = Enum.Platform.None;
 pcall(function() DevicePlatform = UserInputService:GetPlatform(); end);
 IsMobile = (DevicePlatform == Enum.Platform.Android or DevicePlatform == Enum.Platform.IOS);
-local UIRepo = 'https://raw.githubusercontent.com/Skeleton19/LinoriaLib/main/'
-local ScriptRepo = 'https://raw.githubusercontent.com/Skeleton19/funky-friday-autoplay/main/'
+local UIRepo = 'https://raw.githubusercontent.com/Bart3kk/LinLib/main/'
+local ScriptRepo = 'https://raw.githubusercontent.com/Bart3kk/funky-friday-autoplay/main/'
 
 local UI = loadstring(game:HttpGet(UIRepo .. 'Library.lua'))()
 
