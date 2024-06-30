5337b09060b2a71b2c2e1a999fb47424f91b0659
Bart3kk <115643187+Bart3kk@users.noreply.github.com>
2024-06-30 20:31:53 +0200

    Update script.lua

--- script.lua
+++ script.lua
@@ -99,12 +99,11 @@ pcall(function() DevicePlatform = UserInputService:GetPlatform(); end);
 IsMobile = (DevicePlatform == Enum.Platform.Android or DevicePlatform == Enum.Platform.IOS);
 local UIRepo = 'https://raw.githubusercontent.com/Bart3kk/LinLib/main/'
 local ScriptRepo = 'https://raw.githubusercontent.com/Bart3kk/funky-friday-autoplay/main/'
-local UI = nil
 
 if IsMobile then
-    UI = loadstring(game:HttpGet(UIRepo .. 'MobileLibrary.lua'))
+    local UI = loadstring(game:HttpGet(UIRepo .. 'MobileLibrary.lua'))
 else
-    UI = loadstring(game:HttpGet(UIRepo .. 'Library.lua'))()
+    local UI = loadstring(game:HttpGet(UIRepo .. 'Library.lua'))()
 
 local metadata = loadstring(game:HttpGet(ScriptRepo .. 'metadata.lua'))()
 local httpService = game:GetService('HttpService')
